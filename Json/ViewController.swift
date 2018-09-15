0d40d4bed76cd8db42055e91ab9da36f58b549a1
liroy y <liroy.yarimi@gmail.com>
2018-09-15 13:33:55 +0300

    make a json request

--- Json/ViewController.swift
+++ Json/ViewController.swift
@@ -8,16 +8,58 @@
 
 import UIKit
 
+struct WebsiteDescription : Decodable{
+    let name : String
+    let description: String
+    let courses : [Cource]
+}
+
+struct Cource :Decodable{
+    let id: Int? //the qustion mark is for Missing Fields URL
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let jsonUrlStringWebsiteDescription = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+//        let jsonUrlStringMissingFields = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+//        let jsonUrlStringCourses = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+//        let jsonUrlStringCourse = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        
+        guard let url = URL(string: jsonUrlStringWebsiteDescription) else
+        {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil{
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("data is nil")
+                return
+            }
+            do{
+                
+                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+                print("name: \(websiteDescription.name)\ndescription: \(websiteDescription.description)")
+                for i in websiteDescription.courses{
+                    if let id = i.id{
+                        print("id: \(id)\tname: \(i.name!)\tlink: \(i.link!)\timageUrl: \(i.imageUrl!)\n")
+                    }
+                }
+                
+
+            } catch let jsonErr{
+                print("Error serializing json:",jsonErr)
+            }
+            
+        }.resume()
     }
 
 
