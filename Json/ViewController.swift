9ccc9da13328498b2bdf139153a557876da68364
liroy y <liroy.yarimi@gmail.com>
2018-09-15 10:12:51 +0300

    Initial Commit

---
+++ Json/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Json
+//
+//  Created by liroy yarimi on 15.9.2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
