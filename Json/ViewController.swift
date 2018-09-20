43180932262788dad77af8ea96d2a4708b27ed29
liroy y <liroy.yarimi@gmail.com>
2018-09-20 11:17:48 +0300

    order the code and add explanation

--- Json/ViewController.swift
+++ Json/ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-struct WebsiteDescription : Decodable{
+struct WebsiteDescription : Decodable{ //:Decodable - for our new way
     let name : String
     let description: String
     let courses : [Cource]
 }
 
-struct Cource :Decodable{
+struct Cource :Decodable{ //:Decodable - for our new way
     let id: Int? //the qustion mark is for Missing Fields URL
     let name: String?
     let link: String?
     let imageUrl: String?
+    
+    //this is the old way to do it
+//    init(json: [String:Any]) {
+//        id = json["id"] as? Int ?? -1
+//        name = json["name"] as? String ?? ""
+//        link = json["link"] as? String ?? ""
+//        imageUrl = json["imageUrl"] as? String ?? ""
+//    }
 }
 
 class ViewController: UIViewController {
@@ -27,41 +35,111 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
 
-        let jsonUrlStringWebsiteDescription = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
-//        let jsonUrlStringMissingFields = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
-//        let jsonUrlStringCourses = "https://api.letsbuildthatapp.com/jsondecodable/courses"
-//        let jsonUrlStringCourse = "https://api.letsbuildthatapp.com/jsondecodable/course"
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
         
-        guard let url = URL(string: jsonUrlStringWebsiteDescription) else
-        {return}
+        //check for urlJson
+        guard let url = URL(string: jsonUrlString) else
+        {
+            print("url is nil")
+            return}
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
+            //check error
             if error != nil{
                 print(error!)
                 return
             }
+            //check data
             guard let data = data else {
                 print("data is nil")
                 return
             }
-            do{
-                
-                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
-                print("name: \(websiteDescription.name)\ndescription: \(websiteDescription.description)")
-                for i in websiteDescription.courses{
-                    if let id = i.id{
-                        print("id: \(id)\tname: \(i.name!)\tlink: \(i.link!)\timageUrl: \(i.imageUrl!)\n")
-                    }
-                }
+//            do{//start working
+            
+//            print data as a string
+//            self.printDataAsString(data: data)
+//
+//            this is the old way to do it
+//            self.theOldWay(data: data)
+//
+//            cource call
+//            self.courceCall(data: data)
+//
+//            cources call
+//            self.courcesCall(data: data)
+//
+//            website description call
+//            self.websiteDescriptionCall(data: data)
+//
+            //courses missing fields call is like courcesCall but we need to add questions mark '?' to Cource struct
+            self.courcesCall(data: data)
                 
 
-            } catch let jsonErr{
-                print("Error serializing json:",jsonErr)
-            }
+                
+
+//            } catch let jsonErr{
+//                print("Error serializing json:",jsonErr)
+//            }
             
         }.resume()
     }
+    
+    func websiteDescriptionCall(data: Data){
+        do{
+            let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+//            print(websiteDescription.name, websiteDescription.description)
+            print("name: \(websiteDescription.name)\ndescription: \(websiteDescription.description)")
+            for i in websiteDescription.courses{
+                if let id = i.id{
+                    print("id: \(id)\tname: \(i.name!)\tlink: \(i.link!)\timageUrl: \(i.imageUrl!)\n")
+                }
+            }
 
+        } catch let jsonErr{
+            print("Error serializing json:",jsonErr)
+        }
+    }
+    
+    func courcesCall(data:Data){
+        do{
+            let cources = try JSONDecoder().decode([Cource].self, from: data)
+            print(cources)
+            print("item 1: \(cources[1].name!)")
+        } catch let jsonErr{
+            print("Error serializing json:",jsonErr)
+        }
+        
+    }
+    
+    func courceCall(data:Data){
+        do{
+            let cource = try JSONDecoder().decode(Cource.self, from: data)
+            print(cource)
+        } catch let jsonErr{
+            print("Error serializing json:",jsonErr)
+        }
+        
+    }
+    
+//    //this is the old way to do it
+//    func theOldWay(data: Data){
+//        do{
+//            guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else{ return}
+//            let cource = Cource(json: json)
+//            print(cource.name)
+//        } catch let jsonErr{
+//            print("Error serializing json:",jsonErr)
+//        }
+//    }
+
+    //print data as a string
+    func printDataAsString(data : Data){
+        let dataAsString = String(data: data ,encoding: .utf8)
+        print(dataAsString!)
+    }
 
 }
 
